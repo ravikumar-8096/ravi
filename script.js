31751e32a8351af1348718c20e0197fd51e9bb34
ravikumar <36152528+ravikumar8096@users.noreply.github.com>
2021-02-24 22:34:20 +0530

    new commit
    changes done

--- script.js
+++ script.js
@@ -1,60 +1,76 @@
-const BODY = document.querySelector("body");
+/*===== MENU SHOW =====*/ 
+const showMenu = (toggleId, navId) =>{
+  const toggle = document.getElementById(toggleId),
+  nav = document.getElementById(navId)
 
-var randomWidth = () => {
-  let x = 0;
-  if (detectMob()) {
-    //    x = getRndInteger(65,85);
-    document.querySelector(".container-box").style.width = "80%";
-    document.querySelector(".container-box").setAttribute("data-value",  " "); 
-      document.querySelector(".btn-stop").style.opacity = "0";
-    clearInterval(interval);
-  } else {
-    x = getRndInteger(30, 70); 
-    document.querySelector(".container-box").style.width = x + "%";
-    document.querySelector(".container-box").setAttribute("data-value", x+ "%"); 
-
+  if(toggle && nav){
+      toggle.addEventListener('click', ()=>{
+          nav.classList.toggle('show')
+      })
   }
-};
+}
+showMenu('nav-toggle','nav-menu')
 
-var getRndInteger = (min, max) => {
-  return Math.floor(Math.random() * (max - min)) + min;
-};
+/*===== REMOVE MENU MOBILE =====*/
+const navLink = document.querySelectorAll('.nav__link')
 
-randomWidth();
+function linkAction(){
+  const navMenu = document.getElementById('nav-menu')
+  navMenu.classList.remove('show')
+}
+navLink.forEach(n => n.addEventListener('click', linkAction))
 
-var interval = setInterval(() => {
-  randomWidth();
-}, 2000);
+/*===== SCROLL SECTIONS ACTIVE LINK =====*/
+const sections = document.querySelectorAll('section[id]')
 
-let stopbtn = document.querySelector(".btn-stop");
+window.addEventListener('scroll', scrollActive)
 
-stopbtn.addEventListener("mouseover", function () {
-  this.style.left = getRndInteger(-10, 99) + "%";
-  this.style.top = getRndInteger(-10, 99) + "%";
-  BODY.style.background = getRandomColor();
-  this.innerHTML = "Stop it :)";
-  this.style.bottom = "auto"; 
-});
+function scrollActive(){
+  const scrollY = window.pageYOffset
 
-stopbtn.addEventListener("click", function () {
-  clearInterval(interval);
-  BODY.style.background = "white"; 
-  this.style.opacity = "0";
-});
+  sections.forEach(current =>{
+      const sectionHeight = current.offsetHeight
+      const sectionTop = current.offsetTop - 50;
+      sectionId = current.getAttribute('id')
 
-function detectMob() {
-  return window.innerWidth <= 1000 && window.innerHeight <= 1000;
+      if(scrollY > sectionTop && scrollY <= sectionTop + sectionHeight){
+          document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.add('active')
+      }else{
+          document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.remove('active')
+      }
+  })
 }
 
-function getRandomColor() {
-  var letters = '0123456789ABCDEF';
-  var color = '#';
-  for (var i = 0; i < 6; i++) {
-    color += letters[Math.floor(Math.random() * 16)];
-  }
-  return color;
-}
+/*===== SCROLL REVEAL ANIMATION =====*/
+const sr = ScrollReveal({
+  origin: 'top',
+  distance: '80px',
+  duration: 1000,
+  reset: true
+})
 
-document.querySelector("#date_").innerHTML =(new Date()).getFullYear();
-// document.querySelector(".footer").style.marginTop = (window.innerHeight/2) - 250 +"px";
-// console.log(window.innerHeight)
+/*SCROLL HOME*/
+sr.reveal('.home__title', {})
+sr.reveal('.home__scroll', {delay: 200})
+sr.reveal('.home__img', {origin:'right', delay: 400})
+
+/*SCROLL ABOUT*/
+sr.reveal('.about__img', {delay: 500})
+sr.reveal('.about__subtitle', {delay: 300})
+sr.reveal('.about__profession', {delay: 400})
+sr.reveal('.about__text', {delay: 500})
+sr.reveal('.about__social-icon', {delay: 600, interval: 200})
+
+/*SCROLL SKILLS*/
+sr.reveal('.skills__subtitle', {})
+sr.reveal('.skills__name', {distance: '20px', delay: 50, interval: 100})
+sr.reveal('.skills__img', {delay: 400})
+
+/*SCROLL PORTFOLIO*/
+sr.reveal('.portfolio__img', {interval: 200})
+
+/*SCROLL CONTACT*/
+sr.reveal('.contact__subtitle', {})
+sr.reveal('.contact__text', {interval: 200})
+sr.reveal('.contact__input', {delay: 400})
+sr.reveal('.contact__button', {delay: 600})
